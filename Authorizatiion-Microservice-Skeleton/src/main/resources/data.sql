68147ed539e482a2b5225ac3c76cc9cb54a4c028
sriharish252 <srihari252000@gmail.com>
2021-07-07 17:31:56 +0530

    Initial Push

---
+++ Authorizatiion-Microservice-Skeleton/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into users (id, password, user_name) values (1, '1234', 'admin');
+insert into users (id, password, user_name) values (2, '1234', 'user1');
+insert into users (id, password, user_name) values (3, '1234', 'user2');
