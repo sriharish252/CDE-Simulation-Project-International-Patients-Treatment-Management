87a02e406bae8a7eceb11a08ca6db78823022191
Kamalesh8 <61987733+Kamalesh8@users.noreply.github.com>
2021-07-07 18:38:27 +0530

    Add files via upload

---
+++ Portal-Microservice-Skeleton/src/main/java/com/cts/portal/model/GetPackage.java
@@ -0,0 +1,12 @@
+package com.cts.portal.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor 
+public class GetPackage {
+	private int pid;
+	private String treatmentPackageName;
+}
