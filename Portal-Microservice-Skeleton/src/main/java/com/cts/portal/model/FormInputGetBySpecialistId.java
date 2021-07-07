27628827b0dfe75bcddfcc6e868b80aae2ff9b59
sriharish252 <srihari252000@gmail.com>
2021-07-07 17:35:36 +0530

    Initial Push

---
+++ Portal-Microservice-Skeleton/src/main/java/com/cts/portal/model/FormInputGetBySpecialistId.java
@@ -0,0 +1,11 @@
+package com.cts.portal.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor
+public class FormInputGetBySpecialistId {
+	private int specialistId;
+}
